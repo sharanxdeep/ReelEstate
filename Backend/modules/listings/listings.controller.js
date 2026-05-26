69265e55a631b989f81123ba6bc6410fd504e233
Sharan <sharandeeps225@gmail.com>
2026-05-26 11:25:00 +0530

    implemented CRUD APIs for the listing module

--- Backend/modules/listings/listings.controller.js
+++ Backend/modules/listings/listings.controller.js
@@ -13,4 +13,51 @@ const createListing = async (req,res)=>{
     }
 }
 
-module.exports = { createListing }
+const getAllListings = async(req,res)=>{
+    try{
+        const listings = await listingModel.find({status : 'live'})
+        res.status(200).json({listings})
+    }
+    catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+const getListing = async(req,res)=>{
+    try{
+        const listing = await listingModel.findById(req.params.id)
+        if (!listing) return res.status(404).json({message:"Listing not found"})
+        res.status(200).json({listing})
+    }
+    catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+const deleteListing = async(req,res)=>{
+    try{
+        const listing = await listingModel.findById(req.params.id)
+        if (!listing) return res.status(404).json({message:"Listing not found"})
+        if(listing.seller.toString()!==req.user._id.toString()) return res.status(403).json({message:"You are not authorized to delete this listing"})
+        await listing.deleteOne()
+        res.status(200).json({message:"Listing deleted"})
+    }
+    catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+const updateListing = async(req,res)=>{
+    try{
+        const listing = await listingModel.findById(req.params.id)
+        if (!listing) return res.status(404).json({message:"Listing not found"})
+        if(listing.seller.toString()!==req.user._id.toString()) return res.status(403).json({message:"You are not authorized to delete this listing"})
+        const updatedListing = await listingModel.findByIdAndUpdate(req.params.id, req.body, {new : true})
+        res.status(200).json({message:"Listing updated",listing : updatedListing})
+    }
+    catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+module.exports = { createListing, getAllListings, getListing, deleteListing, updateListing}
--- Backend/modules/listings/listings.routes.js
+++ Backend/modules/listings/listings.routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const verifyToken = require('../../middleware/auth.middleware')
-const {createListing} = require('./listings.controller')
+const {createListing, getAllListings, getListing, deleteListing, updateListing} = require('./listings.controller')
 
 router.post('/', verifyToken, createListing)
+router.get('/', getAllListings)
+router.get('/:id', getListing)
+router.delete('/:id', verifyToken, deleteListing)
+router.patch('/:id', verifyToken, updateListing)
 
 module.exports = router
